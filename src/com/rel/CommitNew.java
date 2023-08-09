b8b930ebc8f3fdb51145d8c72d6446c6e87d51d3
meetgopikrish <draftmegopikrish>
2023-08-09 12:44:02 +0530

    newproject

---
+++ src/com/rel/CommitNew.java
@@ -0,0 +1,21 @@
+package com.rel;
+
+public class CommitNew {
+
+	private void method1() {
+System.out.println("method 1");
+	}
+	
+	private void method2() {
+		System.out.println("method 2");
+
+	}
+	
+	public static void main(String[] args) {
+		
+	
+	CommitNew cn = new CommitNew();
+	cn.method1();
+	cn.method2();
+}
+}
